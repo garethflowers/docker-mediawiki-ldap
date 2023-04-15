f65400f9e88f48a74f05dbf7a785d1d34e0a6579
Gareth Flowers <garethflowers@users.noreply.github.com>
2023-04-15 01:19:34 +0100

    chore(test): use plain output when building docker

--- test/test.sh
+++ test/test.sh
@@ -2,7 +2,7 @@
 set -ex
 export IMAGE_NAME=garethflowers/mediawiki-ldap
 
-docker build --tag $IMAGE_NAME .
+docker build --progress plain --tag $IMAGE_NAME .
 docker run --rm $IMAGE_NAME php -v
 
 echo "\nOK"
